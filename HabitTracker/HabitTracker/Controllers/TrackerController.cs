86839182edc868c16e2facc680c098734117013b
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-11-29 15:08:04 -0500

    Controllers created

--- HabitTracker/HabitTracker/Controllers/TrackerController.cs
+++ HabitTracker/HabitTracker/Controllers/TrackerController.cs
@@ -3,9 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HabitTracker.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("tracker")]
     [ApiController]
     public class TrackerController : ControllerBase
     {
+        [HttpGet]
+        public List<Tracker> GetAll()
+        {
+            return DAL.GetAllTrackers();
+        }
+
+        [HttpPost]
+        public Tracker Add(Tracker track)
+        {
+            return DAL.AddTracker(track);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            DAL.DeleteTracker(id);
+        }
     }
 }
