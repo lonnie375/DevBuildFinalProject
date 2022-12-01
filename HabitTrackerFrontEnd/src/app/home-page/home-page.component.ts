8712a8944ba3e96f7d6c2295a17db6fb3055be16
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-12-01 09:28:47 -0500

    merged from main

--- HabitTrackerFrontEnd/src/app/home-page/home-page.component.ts
+++ HabitTrackerFrontEnd/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from '../user';
 import { UserService } from '../user.service';
 
@@ -13,7 +14,11 @@ export class HomePageComponent implements OnInit {
 
   selectedId: number = 0;
   selectedUsername: string = '';
-  
+
+  enteredUserName: string = '';
+
+  loginError: boolean = false;
+    
   user: User = {
     id: 0,
     username: ''
@@ -21,21 +26,40 @@ export class HomePageComponent implements OnInit {
 
   @Output() sendId:EventEmitter<number> = new EventEmitter<number>();
   
-  constructor(private UserSrv: UserService) {
+  constructor(private UserSrv: UserService, private router: Router) {
+
+   }
+
+
+  ngOnInit(): void {
+    this.refresh()
+  }
+
+  refresh(){
     this.UserSrv.getAllUsers(
       (result: User[]) => {
         this.UserList = result;
       }
     )
-   }
-
-
-  ngOnInit(): void {
-
   }
 
-  logIn(){
-    this.sendId.emit(this.selectedId);
 
+  logIn(){
+    for(let index: number = 0; index < this.UserList.length; index++){
+      if (this.UserList[index].username == this.enteredUserName){
+        this.selectedId = this.UserList[index].id
+      }
+      else {
+        this.loginError = true;
+      }
+    }
+    
+    if (this.loginError == false){
+      this.sendId.emit(this.selectedId);
+      this.router.navigate(['habits']);
+    }
+    else {
+      this.router.navigate(['']);
+    }
   }
 }
